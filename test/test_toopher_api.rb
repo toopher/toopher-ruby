72061cf3204f421d98b3e715e3a74b9ac4d16c80
Seth Holloway <smholloway@gmail.com>
2013-08-12 21:19:25 -0500

    Add basic tests for SSL

--- test/test_toopher_api.rb
+++ test/test_toopher_api.rb
@@ -5,7 +5,6 @@ require 'toopher_api'
 
 class TestToopher < Test::Unit::TestCase
   def test_constructor()
-
     assert_raise ArgumentError do
       api = ToopherAPI.new
     end
@@ -21,7 +20,6 @@ class TestToopher < Test::Unit::TestCase
     assert_nothing_raised do
       api = ToopherAPI.new('key', 'secret')
     end
-
   end
 
   def test_create_pairing_immediate_success()
@@ -43,8 +41,8 @@ class TestToopher < Test::Unit::TestCase
       assert(pairing.enabled == true, 'pairing not enabled')
       assert(pairing.user_id == '1', 'bad user id')
       assert(pairing.user_name == 'user', 'bad user name')
-
   end
+
   def test_get_pairing_status()
     stub_http_request(:get, "https://toopher-api.appspot.com/v1/pairings/1").
       to_return(
@@ -134,5 +132,22 @@ class TestToopher < Test::Unit::TestCase
     assert_raise ToopherApiError do
       auth = toopher.get_authentication_status('1')
     end
+  end
+
+  def test_toopher_api_returns_ssl_error()
+    WebMock.allow_net_connect!
+    key = ENV['TOOPHER_CONSUMER_KEY']
+    secret = ENV['TOOPHER_CONSUMER_SECRET']
+
+    toopher = ToopherAPI.new(key, secret)
+    assert_nothing_raised do
+      toopher.get_pairing_status('0b56d487-9674-4ba9-a3c3-91b833806c2b')
+    end
+
+    toopher = ToopherAPI.new(key, secret, {}, 'https://sethholloway.com/')
+    assert_raise OpenSSL::SSL::SSLError do
+      toopher.get_pairing_status('0b56d487-9674-4ba9-a3c3-91b833806c2b')
+    end
+    WebMock.disable_net_connect!
   end
 end
